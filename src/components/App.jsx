c8db8d1f21c0c7ab1ce69b5b4eac87ff6416ab5a
DarinaMerkulova <litovchenko.darina@gmail.com>
2023-07-12 00:02:46 +0300

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import HomePage from "pages/HomePage";
+import Movies from "pages/Movies";
+import NotFound from "pages/NotFound";
+import { NavLink, Route, Routes } from "react-router-dom";
+import { styled } from "styled-components";
+
+const StyledLink = styled(NavLink)`
+color: black;
+
+&.active {
+  color: orange;
+}
+`;
 export const App = () => {
+
+  
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/movies">Movies</StyledLink>
+      </nav>
+
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/movies" element={<Movies />} />
+    <Route path="*" element={<NotFound />} />
+    
+  </Routes>
     </div>
   );
 };
---
+++ src/services/fetch.js
@@ -0,0 +1,2 @@
+const KEY = "2d1d8e2963579243d8e1859d5054f040";
+const BASE_URL = "https://api.themoviedb.org/3";
